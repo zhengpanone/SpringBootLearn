1bf015e620d576a2dd4299dc2e646a747d22ad8f
zhengpanone <zhengpanone@hotmail.com>
2022-01-06 09:38:45 +0800

    spring boot validate

--- 12-springboot-validate/src/main/java/com/zp/pojo/User.java
+++ 12-springboot-validate/src/main/java/com/zp/pojo/User.java
@@ -1,40 +1,20 @@
 package com.zp.pojo;
 
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.NotNull;
 
+@Data
 public class User {
 
     private Integer id;
-    @NotBlank(message = "用户名不能为空")
-    @Length(min = 6, max = 12)
+    @NotNull(message = "用户名不能为空")
+    @Length(min = 2, max = 12)
     private String name;
-    @NotBlank(message = "密码不能为空")
+    @NotNull(message = "密码不能为空")
     private Integer age;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
