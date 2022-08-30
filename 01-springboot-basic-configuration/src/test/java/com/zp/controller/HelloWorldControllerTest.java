4fed7fe9c4e49f7c4afafb4652ea12a12ca0d9db
zhengpanone <zhengpanone@hotmail.com>
2022-08-30 20:44:59 +0800

    1、springboot快速入门

--- 01-springboot-basic-configuration/src/test/java/com/zp/controller/HelloWorldControllerTest.java
+++ 01-springboot-basic-configuration/src/test/java/com/zp/controller/HelloWorldControllerTest.java
@@ -1,6 +1,6 @@
 package com.zp.controller;
 
-import com.zp.config.TestConfigBean;
+
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -11,15 +11,15 @@ import static org.junit.Assert.*;   //必须是static
 public class HelloWorldControllerTest {
     HelloWorldController app = new HelloWorldController();
 
+    /**
+     * assertEquals(Object expected, Object actual)
+     * expected 是期望值
+     * actual 是被测代码返回的实际值
+     */
     @Test
     public void testSayHello() {
-        /**
-         * assertEquals(Object expected, Object actual)
-         * expected 是期望值
-         * actual 是被测代码返回的实际值
-         */
         Map<String, String> map = new HashMap<>();
-        map.put("msg","HelloWorld!");
+        map.put("msg", "HelloWorld!");
         assertEquals(map, new HelloWorldController().sayHello());
     }
 
